4f1b819b8cfd00f17fec01677b1b95bd6546adfc
Tarun Prajapati <tarun.praj@outlook.com>
2024-03-24 23:55:21 +0530

    fred api

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-print("Hi")
+from fredapi import Fred
+import matplotlib.pyplot as plt
+
+fred_key = "a85f52ab102ccf808744766693f330b4"
+
+fred = Fred(api_key = fred_key)
+
+sample_search = fred.search("india gdp")
+
+print(sample_search)
+
+
+def unemp_data(id='SLUEM1524ZSIND'):
+    unemp = fred.get_series(id)
+    unemp.plot()
+    plt.show()
+
+unemp_data()
 
